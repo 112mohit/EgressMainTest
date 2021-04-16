c82fa25715b7e429f29990bef198adc7a7cbe89b
Mohit Sharma <112mohit@gmail.com>
2021-04-16 12:53:58 +0100

    Updated 2

--- EgressMainClass/AuthClass.cs
+++ EgressMainClass/AuthClass.cs
@@ -17,20 +17,21 @@ namespace EgressMainClass
             
             var restClient = new RestClient("https://egressassessmentapi20210323143024.azurewebsites.net");
 
-            var restRequest = new RestRequest("/egress/authenticate", Method.POST);
+            //Auth 
+            var restRequest = new RestRequest("/egress/authenticate", Method.POST); //Post method to pass username and pass
+
+            //Adding username and pass to Json Body
             restRequest.AddJsonBody(new { username = "user", password = "test1234" });
 
             restRequest.RequestFormat = DataFormat.Json;
             var response = restClient.Execute(restRequest);
 
+            //Getting Token
             var deserialise = new JsonDeserializer();
             var output = deserialise.Deserialize<Dictionary<string, string>>(response);
             var token = output["token"];
 
             authToken = token.ToString();
-
-            //restRequest.AddHeader("Authorization", "Bearer " + authToken);
-
 
 
         }
